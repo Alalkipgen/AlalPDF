3b09533db874355590934604c3d5b0f4c1a2bfbe
Alalkipgen <47500896+Alalkipgen@users.noreply.github.com>
2026-09-19 00:33:59 +0630

    fix: add missing Row and TextButton imports

--- app/src/main/java/com/alalkipgen/alalpdf/library/LibraryScreen.kt
+++ app/src/main/java/com/alalkipgen/alalpdf/library/LibraryScreen.kt
@@ -2,6 +2,7 @@ package com.alalkipgen.alalpdf.library
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
