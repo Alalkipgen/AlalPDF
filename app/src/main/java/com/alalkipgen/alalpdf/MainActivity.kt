4bdf16a09d645bd1633764c5e53840465fc1b18b
Alalkipgen <47500896+Alalkipgen@users.noreply.github.com>
2026-09-18 23:10:35 +0630

    fix: remove invalid roundToPx import

--- app/src/main/java/com/alalkipgen/alalpdf/MainActivity.kt
+++ app/src/main/java/com/alalkipgen/alalpdf/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.roundToPx
 import androidx.compose.material3.Text
 import com.alalkipgen.alalpdf.ui.theme.AlalPdfTheme
 import com.alalkipgen.alalpdf.library.LibraryScreen
